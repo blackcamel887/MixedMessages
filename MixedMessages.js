5dbe42e9d580ece15fb42b47a16d6e2d3048f3ed
Blackcamel887 <Pajamas887.com>
2023-06-21 01:04:09 -0700

    Completed program

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,3 +1,14 @@
 let names = ["Adrian","Bob","Charlie","Darren","Emily","Fanny","Garen","Henry","Isabelle",
 "John","Kristin","Langdon","Mary","Nick","Owen","Prisha","Robert","Queen","Steven","Timmy","Uvana",
 "Vex","Winson","Xenith","Yin","Zana"];
+let adjectives = ["funny","scary","friendly","nice","easy-going","smart","good-looking",
+"amicable","strict","helpful","hardworking", "awesome", "one of a kind"];
+let occupations = ["police officer","lunch lady", "teacher", "friend", "person", "student", "CEO", "plumber", "neighbor",
+"landlord", "gardener", "lawyer", "child", "adult", "king", "prince", "princess", "technician"];
+const choose = (arr) =>{
+    return Math.floor(Math.random()*arr.length);
+}
+let name = choose(names);
+let adjective = choose(adjectives);
+let occupation = choose(occupations);
+console.log(`${name} is a ${adjective} ${occupation}`);
