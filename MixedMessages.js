440f0c1af339a4cb26598a75d80639a070a6dc35
Blackcamel887 <Pajamas887.com>
2023-06-21 00:54:42 -0700

    added names array with a name for each letter

---
+++ MixedMessages.js
@@ -0,0 +1,3 @@
+let names = ["Adrian","Bob","Charlie","Darren","Emily","Fanny","Garen","Henry","Isabelle",
+"John","Kristin","Langdon","Mary","Nick","Owen","Prisha","Robert","Queen","Steven","Timmy","Uvana",
+"Vex","Winson","Xenith","Yin","Zana"];
